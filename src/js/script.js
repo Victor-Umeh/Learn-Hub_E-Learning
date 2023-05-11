957f94c72c188eac7ec330c671a4f854d4d99556
tosin <tosinekshally@gmail.com>
2023-05-11 17:46:52 +0100

    added functionalities

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
 let sideBar  = document.getElementById("modal")
 let signUpBtn = document.getElementById("signBtn")
 function options(){
+  if(sideBar.style.display==="block"){
+    sideBar.style.display = "none";
+}
+else{
   sideBar.style.display = "block";
+}
 }
