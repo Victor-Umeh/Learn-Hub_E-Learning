d396eb41398fb284fc20c366589583228eb34b05
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-05-16 16:00:07 +0100

    Fix errors

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+let sideBar  = document.getElementById("modal")
+let signUpBtn = document.getElementById("signBtn")
+let closed = document.getElementById("close")
+function options(){
+  if(sideBar.style.display==="block"){
+    sideBar.style.display = "none";
+}
+else{
+  sideBar.style.display = "block";
+}
+}
