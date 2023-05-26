6853c1de10f89967c0030e0541213169d16c18fb
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-05-26 12:13:37 +0100

    Edit quiz nav

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -295,3 +295,27 @@ function updateTimer() {
 
 // Start the timer
 setInterval(updateTimer, 1000);
+
+
+const storedUser = localStorage.getItem("data")
+const user = JSON.parse(storedUser)
+console.log(user.firstname)
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the userName element
+  const userNameElement = document.getElementById("userName");
+  const profileFirstNmae = document.getElementById("profileFirstNmae")
+  const profileLast = document.getElementById("lastName")
+  const profileemail = document.getElementById("profileEmail")
+
+  if (userNameElement) {
+    userNameElement.innerHTML = user.firstname; 
+    profileFirstNmae.innerHTML = user.firstname;
+  }
+  if (profileemail){
+    profileemail.innerHTML = user.email
+  }
+  if (profileLast){
+    profileLast.innerHTML = user.lastname
+  }
+});
